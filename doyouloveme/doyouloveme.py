17b13d570cfd06eee762b5be1f3cbc5bd7ae80e8
Anismash <animesmash525@gmail.com>
2017-04-19 12:58:48 +0200

    Update doyouloveme.py
    Fixed missing Pictures

--- doyouloveme/doyouloveme.py
+++ doyouloveme/doyouloveme.py
@@ -1,4 +1,7 @@
-﻿from discord.ext import commands
+from discord.ext import commands
+from cogs.utils import checks
+from cogs.utils.chat_formatting import box,  pagify, escape_mass_mentions
+import datetime
 import os
 import random
 import discord
@@ -9,16 +12,16 @@ class doyouloveme:
     def __init__(self, bot):
         self.bot = bot
         self.love = ["Yes, I do! :heart: http://i.imgur.com/cRfX87T.gif",
-                     "Die! http://i.imgur.com/QzfBdVe.gif",
+                     "Die! http://i.imgur.com/VlNUDlc.gif",
                      "I-It's not that I like you or anything... http://i.imgur.com/b0RWwM2.gif",
                      "If you bow down and kiss my feet, {} http://i.imgur.com/gD2w8oV.gif",
                      "Nope. http://i.imgur.com/yqKQTef.gif",
                      "That's funny, {} http://i.imgur.com/ZnmFUhR.gif",
-                     "You are scaring me http://i.imgur.com/fEurqrT.gif",
-                     "I don't care what you feel http://i.imgur.com/VhZZUH0.gif",
-                     "We will have much fun http://i.imgur.com/QY5zTs0.gif",
-                     "What is love? http://i.imgur.com/GCLzjTO.gif",
-                     "I will crush your nuts http://i.imgur.com/zCRMpX7.gif",
+                     "You are scaring me http://i.imgur.com/9CznZbn.gif",
+                     "I only need my games http://i.imgur.com/nix5Siw.gif",
+                     "We will have much fun http://i.imgur.com/ofqTvsu.gif",
+                     "What is love? http://i.imgur.com/smvA3cP.gif",
+                     "I will crush your nuts http://i.imgur.com/hoVanq9.gif",
                      "Yes... I love you ~ http://i.imgur.com/KKjHhUV.gif",
                      "B-Baka! Stop asking this! http://i.imgur.com/zde7L1w.gif"]
         self.loveothers = ["Yes, {} loves you :heart:",
@@ -29,13 +32,16 @@ class doyouloveme:
         """Random Love Answer"""
         if ctx.message.author.id == "114338628695621634":
             user = ctx.message.author
-            await self.bot.say("Of course I love you because you are my BF, {} :heart:".format(user.mention))
+            await self.bot.say(random.choice(self.love).format(user.mention))
+            await self.bot.say(", {} :heart:".format(user.mention))
         elif user == None:
             user = ctx.message.author
             await self.bot.say(random.choice(self.love).format(user.mention))
         else: 
             await self.bot.say(random.choice(self.loveothers).format(user.mention))
         
+    async def doyoulovemetest(self, ctx, *, user : discord.Member=None):
+        await self.bot.say("test")
 
 def setup(bot):
     bot.add_cog(doyouloveme(bot))
